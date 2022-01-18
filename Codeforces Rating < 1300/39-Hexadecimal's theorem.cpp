efe2a1898c1049010bc8774872a1ae736983b46a
Rhythm Bansal <45748739+rskbansal@users.noreply.github.com>
2022-01-19 04:12:47 +0530

    Add files via upload

---
+++ Codeforces Rating < 1300/39-Hexadecimal's theorem.cpp
@@ -0,0 +1,16 @@
+//Hexadecimal's theorem
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+  int n;
+  cin>>n;
+  cout<<"0 0 "<<n;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+}
