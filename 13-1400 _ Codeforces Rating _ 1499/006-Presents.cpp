850288589e47a3424b96aff4db91f25238197f9c
Rhythm Bansal <rskbansal@outlook.com>
2022-07-04 12:48:06 +0530

    added Presents.cpp

---
+++ 13-1400 _ Codeforces Rating _ 1499/006-Presents.cpp
@@ -0,0 +1,22 @@
+//Presents
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+  int n,x;
+  cin>>n;
+  int a[n+1];
+  for(int i=1; i<=n; i++)
+  {
+    cin>>x;
+    a[x]=i;
+  }
+  for(int i=1; i<=n; i++) cout<<a[i]<<" ";
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+    solve();
+}
