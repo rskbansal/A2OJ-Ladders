30996e73e18d2530672e2bff56cdd73c83646ee7
Rhythm Bansal <rskbansal@outlook.com>
2022-07-23 12:49:55 +0530

    added 071

---
+++ 11-Codeforces Rating _ 1300/071-Hungry Sequence.cpp
@@ -0,0 +1,16 @@
+//Hungry Sequence
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+  int n;
+  cin>>n;
+  for(int i=n; i<2*n; i++) cout<<i<<" ";
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+}
