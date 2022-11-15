5e145bc4d94f86e0f5116911833d9de98efc41b8
Rhythm Bansal <rskbansal@outlook.com>
2022-11-16 00:08:12 +0530

    corrected code for 098

--- 11-Codeforces Rating _ 1300/098-Sort the Array.cpp
+++ 11-Codeforces Rating _ 1300/098-Sort the Array.cpp
@@ -1,23 +1,37 @@
-//Sort the Array
 #include<bits/stdc++.h>
 using namespace std;
 void solve()
 {
-  int n;
+  int n,s,e;
   cin>>n;
-  int a[n],b[n];
-  for(int i=0; i<n; i++)
+  int a[n],compare[n];
+  for(int i=0; i<n; i++) cin>>a[i];
+  copy(a,a+n,compare);
+  sort(compare,compare+n);
+  if(equal(a,a+n,compare,compare+n)) cout<<"yes\n1 1";
+  else
   {
-    cin>>a[i];
-    b[i]=a[i];
-  }
-  sort(b,b+n);
-  for(int i=0; i<n; i++)
-  {
-    if(a[i]==b[i]) a[i]==0;
+    for(int i=0; i<n; i++)
+    {
+      if(a[i]!=compare[i])
+      {
+        s=i;
+        break;
+      }
+    }
+    for(int i=n-1; i>=0; i--)
+    {
+      if(a[i]!=compare[i])
+      {
+        e=i;
+        break;
+      }
+    }
+    reverse(a+s,a+e+1);
+    if(equal(a,a+n,compare,compare+n)) cout<<"yes\n"<<s+1<<" "<<e+1;
+    else cout<<"no";
   }
 }
-
 int main()
 {
   ios_base::sync_with_stdio(0);
